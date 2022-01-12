59ffd8f929a3563cf435ae05ea45e047396e4d05
Thanos Athens <thanosoncode@gmail.com>
2022-01-12 09:55:50 +0200

    router6

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Navbar from "./Navbar";
 import Cart from "./Cart";
 import Error from "./Error";
 import ProductDetails from "./ProductDetails";
 
-
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/products">
-          <Home />
-        </Route>
-        <Route path="/products/:id">
-          <ProductDetails />
-        </Route>
-        <Route path="/cart">
-          <Navbar />
-          <Cart />
-        </Route>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/products" element={<Home />}></Route>
+        <Route path="/products/:id" element={<ProductDetails />}></Route>
+        <Route
+          path="/cart"
+          element={
+            <>
+              <Navbar />
+              <Cart />
+            </>
+          }
+        ></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
     </Router>
   );
 };
